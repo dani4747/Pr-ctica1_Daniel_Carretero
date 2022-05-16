e437bbaa4d448448d2a6c6b1c217efb2a383e320
dani4747 <98947268+dani4747@users.noreply.github.com>
2022-05-16 12:08:11 +0200

    Update mi_practica1_daniel_carretero.py

--- mi_practica1_daniel_carretero.py
+++ mi_practica1_daniel_carretero.py
@@ -1,3 +1,5 @@
+'''Daniel Carretero'''
+
 '''
 Implementar un merge concurrente:
 - Tenemos NPROD procesos que producen números no negativos de forma
@@ -16,148 +18,109 @@ semáforos.
 los productores ponen valores en el búffer.
 '''
 
-from multiprocessing import Process, Value, Array, current_process, Semaphore, BoundedSemaphore, Lock
+'''
+notas:
+[Lock()]*n
+
+consumidor:
+    wait 'todas prducido'
+    loop -todos los prod han producido-
+        [procesar el minimo] -> pos
+        signal al pos, espero que produzca (wait pos)
+    
+consumidor:
+    wait 'todas prducido'
+    running = [True, True, ... True]
+    while alguno de los valores sea True
+    loop -todos los prod han producido-
+        [procesar el minimo] -> pos
+        signal al pos, espero que produzca (wait pos)
+
+'''
+
+#importo todas las librerías necesarias
+from multiprocessing import  Process, Value, Array, current_process, Semaphore, BoundedSemaphore, Lock
 from time import sleep
 from random import random,randint
 
-N = 30
-K = N
-NPROD = 3
-NCONS = 1
+N=4
+NPROD=15
+NCONS=3
 
-def delay(factor = 3):
+def delay(factor=3):
     sleep(random()/factor)
-
-
-def add_data(storage, index, pid, data, mutex):
-    mutex.acquire()
-    try:
-        storage[index.value] = randint(1,9) + storage[index.value-1]
-        delay(6)
-        index.value = index.value + 1
-    finally:
-        print(storage[:])
-        mutex.release()
-
-
-def get_data(storagelst, indexlst, mutex, miLista,indice):
-    mutex.acquire()
-    try:
-        li = []
-        for i in range(NPROD):
-            if not (storagelst[i][0] == -1):
-                li.append(storagelst[i][0])
-        minimo = min(li)
-        for i in range(NPROD):
-            if minimo == storagelst[i][0]:
-                indexlst[i].value = indexlst[i].value - 1
-                delay()
-                for j in range(indexlst[i].value):
-                    storagelst[i][j] = storagelst[i][j + 1]
-                storagelst[i][indexlst[i].value] = -1
-                break
-        
-        data = minimo
-        miLista[indice] = data
-    finally:
-        mutex.release()
-        print(storagelst[0][:],storagelst[1][:],storagelst[2][:])
-        print(miLista[:])
     
-def producer(storage, index, empty, non_empty, mutex):
-    for v in range(N):
-        delay(6)
-        print(f"producer {current_process().name} produciendo")
-        
-        empty.acquire()
-        add_data(storage, index, int(current_process().name.split('_')[1]),
-                 v, mutex)
-        non_empty.release()
-        print(f"producer {current_process().name} almacenado {v}")
+def producer(indice, buf, l): # Parámetros de entrada: indices, buffer y lista
+    val=0
+    i=0
+    while (i<N): 
+        val+=randint(0,6)
+        print('Producer num -> ',indice,'Value -> ',val)
+        l[2*indice].acquire() 
+        buf[indice]=val
+        l[2*indice+1].release()
+        i+=1
+    val=-1
+    l[2*indice].acquire() 
+    buf[indice]=val
+    l[2*indice+1].release() 
 
-
-def consumer(storagelst, indexlst, empty, non_empty, mutex, miLista):
-    indice = 0
-    for v in range(N*NPROD):
-        non_empty.acquire()
-        contador = 0
-        while  (( indexlst[0].value == 0 or indexlst[1].value == 0 or indexlst[2].value == 0) and (N*NPROD-indice)>2) and contador < 10:
-            contador += 1
-            delay()
-        dato = get_data(storagelst, indexlst, mutex,miLista,indice)
-        empty.release()
-        indice += 1
-        print(f"consumer {current_process().name} consumiendo {dato}")
+def consumer(buf, l):  # buffer y lista
+    lnum=[]
+    k=0
+    while (k<NPROD):
+        l[2*k+1].acquire()
+        k+=1    
+    while (list(buf)!=[-1]*NPROD):
         delay()
-        
-def ordenar(listas):
-    li = []
-    while(not menos_unos(listas)):
-        li.append(toma_mins(listas))
-    return li
-        
-def toma_mins(listas):
-    li = []
-    for i in listas:
-        if i[0] != -1:
-            li.append(i[0])
-    minimo = min(li)
-    for i in listas:
-        if minimo == i[0]:
-            i.pop(0)
-            break
-    return minimo
-        
-def menos_unos(lista):
-    for i in lista:
-        for j in i:
-            if j!=-1:
-                return False
-    return True
-
+        val, indice=toma_mins(buf)
+        print('New value:',val, 'By producer',indice)
+        lnum.append(val)
+        print(lnum)
+        l[2*indice].release()
+        l[1+2*indice].acquire()
+    print ('List values:',lnum)
+    
+def toma_mins(l): # Toma la lista como entrada
+    l2=[0]*len(l)
+    m=max(l) # Tomo el máximo de la lista
+    for i in range(len(l)):
+        if l[i] == -1: 
+            l2[i]=m+1
+        else:
+            l2[i]=l[i]        
+    mi=l2[0] #minimo 
+    indice=0 
+    #Busco el elemento mas pequeño
+    for i in range(1, len(l2)):
+        if l2[i] != -1 and mi>l2[i]:
+            mi=l2[i]
+            indice=i        
+    return mi, indice  
+  
+# Implementamos el main
 def main():
-
-    non_empty = Semaphore(0)
-    empty = BoundedSemaphore(K)
-    mutex = Lock()
+    sem=[]
+    buf=Array('i', NPROD)
     
-    indexlst = [Value('i',0) for _ in range(NPROD)]
-        
-    storagelst = [Array('i',K) for _ in range(NPROD)]
+    k=0
+    while k<NPROD:
+        sem.append(BoundedSemaphore(1))
+        sem.append(Semaphore(0)) 
+        k+=1
     
-    for j in storagelst:
-            for i in range(K):
-                j[i] = -1
+    procesos=[]  
+    for indice in range(NPROD):
+        procesos.append(Process(target=producer, args=(sem, buf, indice)))
+    procesos.append(Process(target=consumer, args=(sem, buf)))    
+    for proceso in procesos:
+        proceso.start()
+    for proceso in procesos:
+        proceso.join()
+
+if __name__ == "__main__":
+    main()    
+           
+
+
     
-    miLista = Array('i',N*3)
-    
-    prodlst = [ Process(target=producer,
-                        name=f'prod_{i}',
-                        args=(storagelst[i], indexlst[i], empty, non_empty, mutex))
-                for i in range(NPROD) ]
-
-    conslst = [ Process(target=consumer,
-                      name=f"cons_{i}",
-                      args=(storagelst, indexlst, empty, non_empty, mutex,miLista))
-                for i in range(NCONS) ]
-
-    for p in prodlst:
-        p.start()
-    
-    for p in conslst:
-        p.start()
-
-    for p in prodlst:
-        p.join()
-    
-    for p in  conslst:
-        p.join()
-        
-    print(miLista[:],len(miLista[:]))
-
-
-if __name__ == '__main__':
-    main()
-
-
-        
